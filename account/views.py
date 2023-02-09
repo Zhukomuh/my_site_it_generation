926e03841925958e543c5425a7067681f4fdbf1a
ZhukoMuh <golosenko95@gmail.com>
2023-02-09 15:28:01 +0200

    add db

--- account/views.py
+++ account/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .forms import UserRegistration
 
 # Create your views here.
+def registration_view(request):
+    form = UserRegistration(request.POST or None)
+    if form.is_valid():
+        pass
+    return render(request)
